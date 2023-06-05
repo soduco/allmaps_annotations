754db1f57e04fe75b7cd2cb87e404cae3c357745
Julien Perret <julien.perret@gmail.com>
2023-06-05 12:05:47 +0200

    removing debug info

--- create_annotations.py
+++ create_annotations.py
@@ -23,9 +23,7 @@ def create_annotation(file_name, input_proj4_string, id, mask_file, output_file_
     csvFile = pandas.read_csv(file_name,usecols=["mapX","mapY",imageX,imageY],comment='#',skip_blank_lines=True)
     features = []
     for index, row in csvFile.iterrows():
-        # logging.debug(f"{row['mapX']}, {row['mapY']} ({row[imageX]}, {row[imageY]})")
         (lat,lon) = transformer.transform(row['mapX'], row['mapY'])
-        # logging.debug(f"{row['mapX']}, {row['mapY']} => {lat}, {lon} ({row[imageX]}, {row[imageY]})")
         features.append({
             "type": "Feature",
             "properties": {
@@ -41,29 +39,22 @@ def create_annotation(file_name, input_proj4_string, id, mask_file, output_file_
     if mask_file:
         conn = ogr.Open(mask_file)
         lyr = conn.GetLayerByName( id )
-        # print(id)
         if lyr is None:
             print('[ ERROR ]: layer name = "%s" could not be found in database "%s"' % ( id, mask_file ))
         else:
             feature = lyr.GetNextFeature()
-            # print(feature)
             geometry = feature.GetGeometryRef()
-            # print(geometry.ExportToWkt())
             (minX,maxX,minY,maxY) = geometry.GetEnvelope()
             #invert Y values
             minY, maxY = -maxY, -minY
             #convert to int
             minX,maxX,minY,maxY=int(minX),int(maxX),int(minY),int(maxY)
-            # print("minX: %d, minY: %d, maxX: %d, maxY: %d" %(minX,minY,maxX,maxY))
             ring = geometry.GetGeometryRef(0)
             point_count = ring.GetPointCount()
             points = []
             for p in iter(range(point_count)):
                 lon, lat, _ = ring.GetPoint(p)
-                # print(ring.GetPoint(p))
-                # print("lon:%d lat:%d"%(lon, lat))
                 points.append(list((str(int(round(lon))),str(int(-round(lat))))))
-                # print(points)
             poly = ' '.join(list(map(','.join, points)))
     if not poly:
         minX = round(csvFile[imageX].min())
@@ -159,212 +150,6 @@ def create_annotation(file_name, input_proj4_string, id, mask_file, output_file_
         "features": features
     }
 
-#def processFromCSV(csv_file,proj4_string,iiif_image_api_version,output_annotation_file,)
-# TODO 
-# add parameters (source or more generic?)
-# a csv file with the correspondances between sheet_number, gcp point file and iiif id?
-def main():
-    logging.basicConfig(level='DEBUG')
-    entries = []
-    source = 'bhdv'
-    output_directory = 'output'
-    # the proj string prior to the latest survey => should be updated!
-    verniquet_proj4_string = "+proj=omerc +gamma=0.00047289 +lonc=2.33652588 +lon_0=2.33652588 +lat_0=48.83635612 +lat_ts=48.83635612 +x_0=0 +y_0=0 +to_meter=1.9490363 +no_defs +ellps=GRS80"
-    atlas_municipal_proj4_string = "+proj=omerc +gamma=0.00047289 +lonc=2.33652533 +lon_0=2.33652533 +lat_0=48.83635864 +lat_ts=48.83635864 +x_0=0 +y_0=0 +no_defs +ellps=GRS80"
-    if source == 'rumsey':
-        output_directory = 'output/rumsey'
-        directory = 'gcps_rumsey/'
-        for sheet_number in range(1, 73):
-            file_name = '{}.jp2.points'.format(sheet_number+10110001)
-            id = 'verniquet_rumsey_{}'.format(str(sheet_number).zfill(2))
-            output_file_name = output_directory + '/{}/{}.json'.format(str(sheet_number).zfill(2),sheet_number+10110001)
-            output_csv_file_name = output_directory + '/{}/{}.jp2.points'.format(str(sheet_number).zfill(2),sheet_number+10110001)
-            sheetIdDf = pandas.read_csv('../uuids_verniquet_stanford.csv',usecols=["yaml_identifier"],skip_blank_lines=True)
-            sheetIdList = sheetIdDf.drop(sheetIdDf.index[[0]])['yaml_identifier'].tolist()
-            url = 'https://www.davidrumsey.com/luna/servlet/iiif/{}'.format(sheetIdList[sheet_number])
-            if not exists(directory + file_name):
-                logging.debug(f"  Skipping missing file: {directory + file_name}")
-            else: 
-                logging.debug(f"  Creating annotation for file: {directory + file_name}")
-                entry = create_annotation(directory + file_name, verniquet_proj4_string, id, None, output_file_name, url, iiif_image_api_version=3, gcp_output=output_csv_file_name)
-                entries.append(entry)
-        items = []
-        for entry in entries:
-            item = {
-                "type": "Annotation",
-                "id": entry['id'],
-                "@context": [
-                    "http://www.w3.org/ns/anno.jsonld",
-                    "http://geojson.org/geojson-ld/geojson-context.jsonld",
-                    "http://iiif.io/api/presentation/3/context.json"
-                ],
-                "motivation": "georeferencing",
-                "target": {
-                    "type": "Image",
-                    "source": entry['source'],
-                    "service": [
-                    {
-                        "@id": entry['url'],
-                        "type": entry['image_type'],
-                        "profile": "http://iiif.io/api/image/3/level2.json"
-                    }
-                    ],
-                    "selector": {
-                    "type": "SvgSelector",
-                    "value": f"<svg width=\"{entry['width']}\" height=\"{entry['height']}\"><polygon points=\"{entry['polygon']}\" /></svg>"
-                    }
-                },
-                "body": {
-                    "type": "FeatureCollection",
-                    "purpose": "gcp-georeferencing",
-                    "transformation": {
-                        "type": "polynomial",
-                        "options": {
-                            "order": 2
-                        }
-                    },
-                    "features": entry['features']
-                }
-            }
-            items.append(item)
-        dictionary = {
-            "type": "AnnotationPage",
-            "@context": [
-                "http://www.w3.org/ns/anno.jsonld"
-            ],
-            "items": items
-        }
-        output_file_name = output_directory + '/annotation_verniquet_rumsey.json'
-        with open(output_file_name, 'w') as output_file:
-            output_file.write(json.dumps(dictionary, indent=2))
-    elif source == 'bnf':
-        for sheet_number in range(1, 73):
-            output_directory = 'output/bnf'
-            directory = 'gcps_bnf/'
-            file_name = 'f{}.jpg.points'.format(sheet_number+2)
-            id = 'verniquet_bnf_{}'.format(str(sheet_number).zfill(2))
-            output_file_name = output_directory + '/{}/f{}.json'.format(str(sheet_number).zfill(2),sheet_number+2)
-            output_csv_file_name = output_directory + '/{}/f{}.jpg.points'.format(str(sheet_number).zfill(2),sheet_number+2)
-            url = 'https://gallica.bnf.fr/iiif/ark:/12148/btv1b53243704g/f{}'.format(sheet_number+2)
-            if not exists(directory + file_name):
-                logging.debug(f"  Skipping missing file: {directory + file_name}")
-            else: 
-                logging.debug(f"  Creating annotation for file: {directory + file_name}")
-                entry = create_annotation(directory + file_name, verniquet_proj4_string, id, None, output_file_name, url, iiif_image_api_version=1,gcp_output=output_csv_file_name)
-                entries.append(entry)
-        items = []
-        for entry in entries:
-            item = {
-                "type": "Annotation",
-                "id": entry['id'],
-                "@context": [
-                    "http://www.w3.org/ns/anno.jsonld",
-                    "http://geojson.org/geojson-ld/geojson-context.jsonld",
-                    "http://iiif.io/api/presentation/3/context.json"
-                ],
-                "motivation": "georeferencing",
-                "target": {
-                    "type": "Image",
-                    "source": entry['source'],
-                    "service": [
-                    {
-                        "@id": entry['url'],
-                        "type": entry['image_type'],
-                    }
-                    ],
-                    "selector": {
-                    "type": "SvgSelector",
-                    "value": f"<svg width=\"{entry['width']}\" height=\"{entry['height']}\"><polygon points=\"{entry['polygon']}\" /></svg>"
-                    }
-                },
-                "body": {
-                    "type": "FeatureCollection",
-                    "purpose": "gcp-georeferencing",
-                    "transformation": {
-                        "type": "polynomial",
-                        "options": {
-                            "order": 2
-                        }
-                    },
-                    "features": entry['features']
-                }
-            }
-            items.append(item)
-        dictionary = {
-            "type": "AnnotationPage",
-            "@context": [
-                "http://www.w3.org/ns/anno.jsonld"
-            ],
-            "items": items
-        }
-        output_file_name = output_directory + '/annotation_verniquet_bnf.json'
-        with open(output_file_name, 'w') as output_file:
-            output_file.write(json.dumps(dictionary, indent=2))
-    elif source == 'bhdv':
-        input_mask = 'masks.gpkg'
-        for sheet_number in range(1, 17):
-            output_directory = 'output/bhdv'
-            directory = 'atlas_municipal_1887/'
-            file_name = 'planche_{}.points'.format(sheet_number)
-            id = 'atlas_municipal_1887_bhdv_{}'.format(str(sheet_number))
-            url = 'https://iiif.geohistoricaldata.org/iiif/3/BHdV_PL_ATL20Ardt_1887_00{}.jpg'.format(str(sheet_number+1).zfill(2))
-            output_file_name = output_directory + '/BHdV_PL_ATL20Ardt_1887_00{}.json'.format(str(sheet_number+1).zfill(2))
-            if not exists(directory + file_name):
-                logging.debug(f"  Skipping missing file: {directory + file_name}")
-            else: 
-                logging.debug(f"  Creating annotation for file: {directory + file_name} with id {id} and url {url} ({output_file_name})")
-                entry = create_annotation(directory + file_name, atlas_municipal_proj4_string, id, directory+input_mask, output_file_name, url, iiif_image_api_version=3,gcp_output=None)
-                entries.append(entry)
-        items = []
-        for entry in entries:
-            item = {
-                "type": "Annotation",
-                "id": entry['id'],
-                "@context": [
-                    "http://www.w3.org/ns/anno.jsonld",
-                    "http://geojson.org/geojson-ld/geojson-context.jsonld",
-                    "http://iiif.io/api/presentation/3/context.json"
-                ],
-                "motivation": "georeferencing",
-                "target": {
-                    "type": "Image",
-                    "source": entry['source'],
-                    "service": [
-                    {
-                        "@id": entry['url'],
-                        "type": entry['image_type'],
-                        "profile": "http://iiif.io/api/image/3/level2.json"
-                    }
-                    ],
-                    "selector": {
-                    "type": "SvgSelector",
-                    "value": f"<svg width=\"{entry['width']}\" height=\"{entry['height']}\"><polygon points=\"{entry['polygon']}\" /></svg>"
-                    }
-                },
-                "body": {
-                    "type": "FeatureCollection",
-                    "purpose": "gcp-georeferencing",
-                    "transformation": {
-                        "type": "polynomial",
-                        "options": {
-                            "order": 2
-                        }
-                    },
-                    "features": entry['features']
-                }
-            }
-            items.append(item)
-        dictionary = {
-            "type": "AnnotationPage",
-            "@context": [
-                "http://www.w3.org/ns/anno.jsonld"
-            ],
-            "items": items
-        }
-        output_file_name = output_directory + '/annotation_atlas_municipal_1887_bhdv.json'
-        with open(output_file_name, 'w') as output_file:
-            output_file.write(json.dumps(dictionary, indent=2))
-
 def service(url,image_type,iiif_version):
     if iiif_version==3:
         return {
@@ -456,4 +241,3 @@ if __name__ == '__main__':
     if len( sys.argv ) > 5:
         input_mask = sys.argv[5]
     createAnnotations(csv_file_name=csv_file,iiif_version=iiif_image_api_version,proj4_string=proj4_string, output_annotation_file=output_annotation_file, input_mask=input_mask)
-    #main()
